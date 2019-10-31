9e92a99158cc122c4b6585f57fdad02e63efd412
Heikki Hellgren <heikki.hellgren@elektrobit.com>
2019-10-31 11:46:59 +0200

    Improve way to check if video is currently playing
    If user pauses video before changing tab the previous implementation
    would resume the video on unfocused tab

--- yt.js
+++ yt.js
@@ -1,10 +1,17 @@
 var previous_tab = 0;
 var yt_tabs = [];
 
-function stop_resume(tabId) {
+function stop(tabId) {
     chrome.tabs.executeScript(tabId, {
         "code" :
-            "var evt = new KeyboardEvent('keydown', {'keyCode': 75, 'which': 75}); document.dispatchEvent(evt);"
+            "if(!document.querySelector('video').paused) { var evt = new KeyboardEvent('keydown', {'keyCode': 75, 'which': 75}); document.dispatchEvent(evt); }"
+    });
+}
+
+function resume(tabId) {
+    chrome.tabs.executeScript(tabId, {
+        "code" :
+            "if(document.querySelector('video').paused) { var evt = new KeyboardEvent('keydown', {'keyCode': 75, 'which': 75}); document.dispatchEvent(evt); }"
     });
 }
 
@@ -12,15 +19,14 @@ function handle_tabs(tabId) {
     chrome.tabs.get(tabId, function(tab) {
         chrome.tabs.get(previous_tab, function(prev) {
             if (!chrome.runtime.lastError) {
-                if (prev !== undefined && prev.url !== undefined &&
-                    prev.url.includes("youtube.com")) {
-                    stop_resume(prev.id);
+                if (prev.url !== undefined && prev.url.includes("youtube.com")) {
+                    stop(prev.id);
                 }
             }
         });
 
         if (tab.url !== undefined && tab.url.includes("youtube.com")) {
-            stop_resume(tab.id);
+            resume(tab.id);
         }
 
         previous_tab = tab.id;
