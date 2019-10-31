ef624329f2a1c5b220ae7617762fee99131216eb
Heikki Hellgren <heikki.hellgren@elektrobit.com>
2019-10-31 13:38:59 +0200

    Add options to disable auto pause/resume

--- yt.js
+++ yt.js
@@ -1,5 +1,14 @@
 var previous_tab = 0;
 var yt_tabs = [];
+var autopause = true;
+var autoresume = true;
+
+function restore_options() {
+    chrome.storage.sync.get({autopause : autopause, autoresume : autoresume}, function(items) {
+        autopause = items.autopause;
+        autoresume = items.autoresume;
+    });
+}
 
 function stop(tabId) {
     chrome.tabs.executeScript(tabId, {
@@ -16,7 +25,7 @@ function resume(tabId) {
 }
 
 function handle_tabs(tabId) {
-    chrome.tabs.get(tabId, function(tab) {
+    if (autopause) {
         chrome.tabs.get(previous_tab, function(prev) {
             if (!chrome.runtime.lastError) {
                 if (prev.url !== undefined && prev.url.includes("youtube.com")) {
@@ -24,8 +33,10 @@ function handle_tabs(tabId) {
                 }
             }
         });
+    }
 
-        if (tab.url !== undefined && tab.url.includes("youtube.com")) {
+    chrome.tabs.get(tabId, function(tab) {
+        if (autoresume && tab.url !== undefined && tab.url.includes("youtube.com")) {
             resume(tab.id);
         }
 
@@ -33,7 +44,11 @@ function handle_tabs(tabId) {
     });
 }
 
-chrome.tabs.onActivated.addListener(function(info) { handle_tabs(info.tabId); });
+chrome.tabs.onActivated.addListener(function(info) {
+    restore_options();
+    handle_tabs(info.tabId);
+});
+
 chrome.tabs.onRemoved.addListener(function(tabId, info) {
     var idx = yt_tabs.indexOf(tabId);
     if (idx > -1) {
