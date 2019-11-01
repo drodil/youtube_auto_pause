fc4def43fcda503841b1f407950873ac19d3a2eb
Heikki Hellgren <heikki.hellgren@elektrobit.com>
2019-11-01 07:34:09 +0200

    Separate scripts for resume/stopping
    Improve the resume and stop functionality

--- yt.js
+++ yt.js
@@ -2,37 +2,32 @@ var previous_tab = 0;
 var autopause = true;
 var autoresume = true;
 
-function stop(tabId) {
-    chrome.tabs.executeScript(tabId, {
-        "code" :
-            "if(!document.querySelector('video').paused) { var evt = new KeyboardEvent('keydown', {'keyCode': 75, 'which': 75}); document.dispatchEvent(evt); }"
-    });
+function stop(tab) {
+    if (tab !== undefined && tab.url !== undefined && tab.url.includes("youtube.com")) {
+        chrome.tabs.executeScript(tab.id, {"file" : "stop.js"});
+    }
 }
 
-function resume(tabId) {
-    chrome.tabs.executeScript(tabId, {
-        "code" :
-            "if(document.querySelector('video').paused) { var evt = new KeyboardEvent('keydown', {'keyCode': 75, 'which': 75}); document.dispatchEvent(evt); }"
-    });
+function resume(tab) {
+    if (tab !== undefined && tab.url !== undefined && tab.url.includes("youtube.com")) {
+        chrome.tabs.executeScript(tab.id, {"file" : "resume.js"});
+    }
 }
 
 function handle_tabs(tabId) {
-    if (autopause) {
+    if (autopause && previous_tab != 0) {
         chrome.tabs.get(previous_tab, function(prev) {
             if (!chrome.runtime.lastError) {
-                if (prev.url !== undefined && prev.url.includes("youtube.com")) {
-                    stop(prev.id);
-                }
+                stop(prev);
             }
         });
     }
+    previous_tab = tabId;
 
     chrome.tabs.get(tabId, function(tab) {
-        if (autoresume && tab.url !== undefined && tab.url.includes("youtube.com")) {
-            resume(tab.id);
+        if (autoresume) {
+            resume(tab);
         }
-
-        previous_tab = tab.id;
     });
 }
 
@@ -46,3 +41,9 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
 });
 
 chrome.tabs.onActivated.addListener(function(info) { handle_tabs(info.tabId); });
+
+chrome.windows.onFocusChanged.addListener(function() {
+    if (autopause) {
+        chrome.tabs.getCurrent(function(cur) { stop(cur); });
+    }
+});
