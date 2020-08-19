96805f88bdf756b1935d7062079e617bf7623c97
Heikki Hellgren <heikki.hellgren@op.fi>
2020-08-19 13:27:48 +0300

    Add possibility to stop on scroll

--- content.js
+++ content.js
@@ -5,6 +5,16 @@ document.addEventListener("visibilitychange", function() {
     }
 }, false);
 
+var observer = new IntersectionObserver(function(entries) {
+    if(entries[0].isIntersecting === true) {
+        chrome.runtime.sendMessage({visible: true}, function() { void chrome.runtime.lastError;  })
+    } else {
+        chrome.runtime.sendMessage({visible: false}, function() { void chrome.runtime.lastError;  })
+    }
+}, {threshold: [0]});
+
+observer.observe(document.querySelector('video'));
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (!('action' in request)) {
         return;
@@ -17,7 +27,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     if (request.action === 'stop' && !videoElement.paused) {
         videoElement.pause();
-
     } else if (request.action === 'resume' && videoElement.paused) {
         videoElement.play();
     }
--- options.js
+++ options.js
@@ -1,7 +1,8 @@
 function save_options() {
     var autopause = document.getElementById('autopause').checked;
     var autoresume = document.getElementById('autoresume').checked;
-    chrome.storage.sync.set({autopause : autopause, autoresume : autoresume}, function() {
+    var scrollpause = document.getElementById('scrollpause').checked;
+    chrome.storage.sync.set({autopause : autopause, autoresume : autoresume, scrollpause: scrollpause}, function() {
         var status = document.getElementById('status');
         status.textContent = 'Saved!';
         setTimeout(function() { status.textContent = ''; }, 1000);
@@ -9,9 +10,10 @@ function save_options() {
 }
 
 function restore_options() {
-    chrome.storage.sync.get({autopause : true, autoresume : true}, function(items) {
+    chrome.storage.sync.get({autopause : true, autoresume : true, scrollpause: false}, function(items) {
         document.getElementById('autopause').checked = items.autopause;
         document.getElementById('autoresume').checked = items.autoresume;
+        document.getElementById('scrollpause').checked = items.scrollpause
     });
 }
 
