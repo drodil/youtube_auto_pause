c080538a7a56d2be73fa8a06cb7d977bb03534e4
Heikki Hellgren <heikki.hellgren@op.fi>
2020-08-19 13:27:48 +0300

    Add possibility to stop on scroll

--- yt.js
+++ yt.js
@@ -1,39 +1,33 @@
 var previous_tab = 0;
 var autopause = true;
 var autoresume = true;
+var scrollpause = false;
+refresh_settings()
 
-function get_shortcut_key(tab) {
-    if (tab === undefined || tab.url === undefined) {
-        return undefined;
-    }
-
-    if (tab.url.includes("youtube.com")) {
-        return 75;
-    }
-
-    // Default to space
-    return 32;
+function refresh_settings() {
+    chrome.storage.sync.get(['autopause', 'autoresume', 'scrollpause'], function(result) {
+        if('autopause' in result) {
+            autopause = result.autopause
+        }
+        if('autoresume' in result) {
+            autoresume = result.autoresume
+        }
+        if('scrollpause' in result) {
+            scrollpause = result.scrollpause
+        }
+    });
 }
 
 function stop(tab) {
-    var key = get_shortcut_key(tab);
-    if (key === undefined) {
-        return;
-    }
-
     chrome.tabs.sendMessage(tab.id, {'action' : 'stop'});
 }
 
 function resume(tab) {
-    var key = get_shortcut_key(tab);
-    if (key === undefined) {
-        return;
-    }
-
     chrome.tabs.sendMessage(tab.id, {'action' : 'resume'});
 }
 
 function handle_tabs(tabId) {
+    refresh_settings()
     if (autopause && previous_tab != 0) {
         chrome.tabs.get(previous_tab, function(prev) {
             if (!chrome.runtime.lastError) {
@@ -68,6 +62,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 });
 
 chrome.windows.onFocusChanged.addListener(function(info) {
+    refresh_settings()
     if (previous_tab != 0) {
         chrome.tabs.get(previous_tab, function(tab) {
             if (tab === undefined || chrome.runtime.lastError) {
@@ -85,19 +80,66 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         return true;
     }
 
-    if (request.minimized && autopause) {
-        chrome.tabs.get(sender.tab.id, function(tab) {
-            if (!chrome.runtime.lastError) {
-                stop(sender.tab);
-            }
-        });
-    } else if (!request.minimized && autoresume) {
-        chrome.tabs.get(sender.tab.id, function(tab) {
-            if (!chrome.runtime.lastError) {
-                resume(sender.tab);
-            }
-        });
+    refresh_settings()
+    if ('minimized' in request) {
+        if (request.minimized && autopause) {
+            chrome.tabs.get(sender.tab.id, function(tab) {
+                if (!chrome.runtime.lastError) {
+                    stop(sender.tab);
+                }
+            });
+        } else if (!request.minimized && autoresume) {
+            chrome.tabs.get(sender.tab.id, function(tab) {
+                if (!chrome.runtime.lastError) {
+                    resume(sender.tab);
+                }
+            });
+        }
+    }
+    if('visible' in request && scrollpause) {
+        if(!request.visible) {
+            chrome.tabs.get(sender.tab.id, function(tab) {
+                if (!chrome.runtime.lastError) {
+                    stop(sender.tab);
+                }
+            });
+        } else {
+            chrome.tabs.get(sender.tab.id, function(tab) {
+                if (!chrome.runtime.lastError) {
+                    resume(sender.tab);
+                }
+
+            });
+        }
     }
 
     return true;
 });
+
+
+chrome.runtime.onInstalled.addListener(installScript);
+
+function installScript(details){
+    let params = {
+        currentWindow: true
+    };
+    chrome.tabs.query(params, function gotTabs(tabs){
+        let contentjsFile = chrome.runtime.getManifest().content_scripts[0].js[0];
+        let matches = chrome.runtime.getManifest().content_scripts[0].matches
+        for (let index = 0; index < tabs.length; index++) {
+            let execute = false;
+            matches.forEach(function(match) {
+                let reg = match.replace(/[.+?^${}()|/[\]\\]/g, '\\$&').replace('*', '.*');
+                if(new RegExp(reg).test(tabs[index].url) === true) {
+                    execute = true;
+                    return
+                }
+            });
+            if(execute) {
+                chrome.tabs.executeScript(tabs[index].id, {
+                    file: contentjsFile
+                });
+            }
+        }
+    });
+}
